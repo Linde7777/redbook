d10fc87f5c01dbc474966f08e3324be01ee7be85
DESKTOP-J247DPC\owo <x@owocloud.net>
2024-04-17 18:19:15 +0800

    feat(user): 密码加密

--- internal/service/user.go
+++ internal/service/user.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"golang.org/x/crypto/bcrypt"
 	"main/internal/domain"
 	"main/internal/repository"
 )
@@ -15,5 +16,10 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 }
 
 func (svc *UserService) Signup(ctx context.Context, user *domain.User) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hash)
 	return svc.repo.Create(ctx, user)
 }
