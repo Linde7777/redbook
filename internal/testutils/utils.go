cabf5621f43357eca0c67c64b1a846666758ff96
DESKTOP-J247DPC\owo <x@owocloud.net>
2024-04-24 19:42:54 +0800

    feat: user handler signup unittest 邮箱格式错误

---
+++ internal/testutils/utils.go
@@ -0,0 +1,24 @@
+package testutils
+
+import (
+	"bytes"
+	"encoding/json"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GenEncryptedPassword(password string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return string(hash)
+}
+
+func ReqStructToHTTPBody(reqStruct any) *bytes.Reader {
+	reqBodyBytes, err := json.Marshal(reqStruct)
+	if err != nil {
+		panic(err)
+	}
+	reqBody := bytes.NewReader(reqBodyBytes)
+	return reqBody
+}
--- internal/web/user_test.go
+++ internal/web/user_test.go
@@ -1,29 +1,19 @@
 package web
 
 import (
-	"bytes"
-	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
-	"golang.org/x/crypto/bcrypt"
 	"main/internal/domain"
 	"main/internal/service"
 	servicemock "main/internal/service/mocks"
+	"main/internal/testutils"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func generateEncryptedPassword(password string) string {
-	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(hash)
-}
-
 func TestUserHandler_Signup(t *testing.T) {
-	email1 := "abc@gmail.com"
-	password1 := "123456789"
-	encryptedPassword1 := generateEncryptedPassword(password1)
 	testCases := []struct {
 		name             string
 		mock             func(ctrl *gomock.Controller) (service.UserService, service.AuthCodeService)
@@ -36,31 +26,48 @@ func TestUserHandler_Signup(t *testing.T) {
 			mock: func(ctrl *gomock.Controller) (service.UserService, service.AuthCodeService) {
 				userService := servicemock.NewMockUserService(ctrl)
 				userService.EXPECT().Signup(gomock.Any(), &domain.User{
-					Email:    email1,
-					Password: password1,
+					Email:    "abc@gmail.com",
+					Password: "123456789",
 				}).Return(domain.User{
 					ID:       1,
-					Email:    email1,
-					Password: encryptedPassword1,
+					Email:    "abc@gmail.com",
+					Password: testutils.GenEncryptedPassword("123456789"),
 				}, http.StatusOK, nil)
 				authCodeService := servicemock.NewMockAuthCodeService(ctrl)
 				return userService, authCodeService
 			},
 			reqBuilder: func(t *testing.T) *http.Request {
 				reqBodyStruct := ReqSignup{
-					Email:           email1,
-					Password:        password1,
-					ConfirmPassword: password1,
+					Email:           "abc@gmail.com",
+					Password:        "123456789",
+					ConfirmPassword: "123456789",
 				}
-				reqBodyBytes, err := json.Marshal(reqBodyStruct)
-				assert.NoError(t, err)
-				reqBody := bytes.NewReader(reqBodyBytes)
-				req := httptest.NewRequest(http.MethodPost, "/v1/user/signup", reqBody)
+				req := httptest.NewRequest(http.MethodPost, "/v1/user/signup",
+					testutils.ReqStructToHTTPBody(reqBodyStruct))
 				req.Header.Set("Content-Type", "application/json")
 				return req
 			},
 			expectedHTTPCode: http.StatusOK,
 			expectedResponse: "signup success",
+		},
+		{
+			name: "邮箱格式错误",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.AuthCodeService) {
+				return nil, nil
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				reqBodyStruct := ReqSignup{
+					Email:           "abc@.com",
+					Password:        "123456789",
+					ConfirmPassword: "123456789",
+				}
+				req := httptest.NewRequest(http.MethodPost, "/v1/user/signup",
+					testutils.ReqStructToHTTPBody(reqBodyStruct))
+				req.Header.Set("Content-Type", "application/json")
+				return req
+			},
+			expectedHTTPCode: http.StatusBadRequest,
+			expectedResponse: "Key: 'ReqSignup.Email' Error:Field validation for 'Email' failed on the 'email' tag",
 		},
 	}
 
